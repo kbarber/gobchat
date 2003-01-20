f8d53d0c87ec965be70e6325403ad6e87bfc9ed2
resinhead <none@none>
2003-01-20 11:40:31 +0000

    Added code to stop and start Gob. The parameters at the top are incorrect and will be aligned with the proper linux placings later.

--- daemon/linux-sysv.sh
+++ daemon/linux-sysv.sh
@@ -6,12 +6,51 @@
 #
 # chkconfig: - 99 01
 
+RUNSERVER=/home/ken/dev/gob-working/gobchat/daemon/gobd
+PIDFILE=/tmp/gob.pid
+RUSER=ken
+    
+if [ ! -f $RUNSERVER ] ; then
+    echo "Cannot find script: $RUNSERVER"
+    exit 1
+fi
+
+start() {
+    echo -n "Starting gob: "
+    if [ ! -f $PIDFILE ] ; then
+        su -c $RUNSERVER $RUSER
+        touch $PIDFILE
+        ps axw | grep server.jar | grep -v grep | awk '{print $1}' > $PIDFILE
+        echo "OK"
+    else
+        echo "A pid file $PIDFILE already exists, is the program already running?"
+    fi
+}
+
+stop() {
+    echo -n "Stopping gob: "
+    if [ -f $PIDFILE ] ; then
+        local line
+        read line < $PIDFILE
+        kill $line
+        rm $PIDFILE
+        echo "OK"
+    else
+        echo "A pid file $PIDFILE does not exist. Perhaps the server is not running?"
+    fi
+}
+
 case "$1" in
     start)
+        start
         ;;
     stop)
+        stop
         ;;
     restart)
+        stop
+        sleep 2
+        start
         ;;
     *)
         echo "Usage: $0 {start|stop|restart}"
