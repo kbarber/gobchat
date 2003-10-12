1716279d12a564b4d6298a2c61bdcabe726bd328
resinhead <none@none>
2003-10-12 06:00:45 +0000

    Needed to add an exit handler that will be threaded outside the normal thread.

---
+++ server/ExitHandler.java
@@ -0,0 +1,23 @@
+/*
+ * ExitHandler.java
+ *
+ * Created on 12 October 2003, 13:09
+ */
+
+package server;
+
+/**
+ *
+ * @author  ken
+ */
+public class ExitHandler extends Thread {
+    
+    /** Creates a new instance of ExitHandler */
+    public ExitHandler() {
+    }
+    
+    public void run() {
+        Main.consoleOutput("Exiting Gob Online Chat");
+    }
+    
+}
