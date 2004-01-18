8c049407517b6546283749538ca2964c1e69039a
resinhead <none@none>
2004-01-18 03:16:41 +0000

    Fixed up GNU Licensing.

--- sh/bob/gob/client/components/PrivTabControl.java
+++ sh/bob/gob/client/components/PrivTabControl.java
@@ -4,6 +4,26 @@
  * Created on 12 October 2003, 18:57
  */
 
+/*
+    Copyright (C) 2003,2004 Ken Barber
+ 
+    This file is part of Gob Online Chat.
+
+    Gob Online Chat is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    Gob Online Chat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gob Online Chat; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 package sh.bob.gob.client.components;
 
 import java.util.HashMap;
--- sh/bob/gob/server/ClientCommand.java
+++ sh/bob/gob/server/ClientCommand.java
@@ -4,6 +4,26 @@
  * Created on 7 December 2002, 23:40
  */
 
+/*
+    Copyright (C) 2003,2004 Ken Barber
+ 
+    This file is part of Gob Online Chat.
+
+    Gob Online Chat is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    Gob Online Chat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Gob Online Chat; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 package sh.bob.gob.server;
 
 import sh.bob.gob.shared.validation.*;
