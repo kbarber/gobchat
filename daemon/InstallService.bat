bfae8481234ec24a2eae35ee3eee694669dce856
resinhead <none@none>
2004-01-31 13:21:46 +0000

    Install and Uninstall service batch files are no longer needed, this has moved back into the installation script.

--- daemon/InstallService.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set GobRoot="C:\docume~1\ken.barber\mydocu~1\mycode~1\gob\gobchat"
-
-cd %GobRoot%\daemon
-GobServerWin -install "Gob Online Chat Server" "c:\j2sdk1.4.2_03\jre\bin\server\jvm.dll" "-Djava.class.path=daemon\server.jar" -start sh.bob.gob.server.Main -params "config\gobd.conf.xml" -err "log\serviceerr.log" -current "%GobRoot%"
--- daemon/UninstallService.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set GobRoot="C:\docume~1\ken.barber\mydocu~1\mycode~1\gob\gobchat"
-
-cd %GobRoot%\daemon
-GobServerWin -uninstall "Gob Online Chat Server"
