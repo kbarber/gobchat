88f22905f9cbccca7a94367cc968e709551169c7
resinhead <none@none>
2004-01-31 05:45:58 +0000

    *** empty log message ***

---
+++ daemon/InstallService.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set GobRoot="C:\docume~1\ken.barber\mydocu~1\mycode~1\gob\gobchat"
+
+cd %GobRoot%\daemon
+GobServerWin -install "Gob Chat Server" "c:\j2sdk1.4.2_03\jre\bin\server\jvm.dll" "-Djava.class.path=daemon\server.jar" -start sh.bob.gob.server.Main -params "config\gobd.conf.xml" -err "log\serviceerr.log" -current "%GobRoot%"
---
+++ daemon/UninstallService.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set GobRoot="C:\docume~1\ken.barber\mydocu~1\mycode~1\gob\gobchat"
+
+cd %GobRoot%\daemon
+GobServerWin -uninstall "Gob Chat Server"
