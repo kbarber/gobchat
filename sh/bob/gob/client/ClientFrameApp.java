b34b3557991eeee715abe956e98ad29f85de8391
resinhead <none@none>
2004-02-10 08:36:19 +0000

    *** empty log message ***

--- sh/bob/gob/client/ClientFrameApp.java
+++ sh/bob/gob/client/ClientFrameApp.java
@@ -1,6 +1,7 @@
 /*
- * ClientApplet.java
+ * ClientFrameApp.java
  *
+ * Created on 8 February 2004, 01:52
  */
 
 /*
@@ -29,40 +30,25 @@ import sh.bob.gob.client.controllers.ClientConnectionControl;
 import sh.bob.gob.client.controllers.GUIControl;
 import sh.bob.gob.client.network.ConnectionInfo;
 
-import javax.swing.JApplet;
+import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import java.util.logging.*;
 import java.awt.Dimension;
 
-
 /**
- * 
- * <p>This is the main JApplet class. From this all other classes are instantiated.
  *
- * <p>GUIControl is responsible for display control. ClientConnectionControl is
- * a network control sub-process.
- *
- * <p>ClientApplet creates a JTabbedPain object, and gives control of this to
- * GUIControl. From here, ClientConnectionControl and GUIControl will work 
- * independently, updating the GUI by itself.
- *
- * <p>ConnectionInfo is a data object, used to hold shared data.
- *
- * @author  Ken Barber
+ * @author  Ken.Barber
  */
-public class ClientApplet extends JApplet {
+public class ClientFrameApp extends javax.swing.JFrame {
     
     private ConnectionInfo conInfo;
     private ClientConnectionControl conControl;
     private GUIControl guiControl;
     private String Hostname;
     
-    /** 
-     * Creates new ClientApp applet. The primary part of the Gob client.
-     */
-    public ClientApplet() {
-        
+    /** Creates new form ClientFrameApp */
+    public ClientFrameApp() {
         /* Set the look and feel of the GUI */
         try {
             /* Update the look and feel with the local look and feel */
@@ -74,9 +60,18 @@ public class ClientApplet extends JApplet {
             System.out.println("Inability to set look and feel: " + e);
         } 
         
-        /* Build all NetBeans generated components */
         initComponents();
         
+        /* Initialise the connection info object */
+        conInfo = new ConnectionInfo();
+        
+        guiControl = new GUIControl(tbMain, "localhost");
+        
+        /* Create a new concontrol object */
+        conControl = new ClientConnectionControl(guiControl, conInfo);
+        
+        guiControl.setClientConnectionControl(conControl);
+        guiControl.displayGUI();
     }
     
     /** This method is called from within the constructor to
@@ -89,63 +84,35 @@ public class ClientApplet extends JApplet {
 
         getContentPane().setLayout(new java.awt.CardLayout());
 
-        setBackground(new java.awt.Color(204, 204, 255));
-        setName("mainapplet");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                exitForm(evt);
+            }
+        });
+
         tbMain.setTabLayoutPolicy(javax.swing.JTabbedPane.SCROLL_TAB_LAYOUT);
         tbMain.setTabPlacement(javax.swing.JTabbedPane.BOTTOM);
         tbMain.setFont(new java.awt.Font("SansSerif", 1, 12));
+        tbMain.setPreferredSize(new java.awt.Dimension(500, 375));
         tbMain.setOpaque(true);
         getContentPane().add(tbMain, "card2");
 
+        pack();
     }//GEN-END:initComponents
-
-    /**
-     * <p>The method first executed after the object has been initiated.
-     *
-     * <p>It intantiates the main three objects, GUIControl and ClientConnectionControl
-     * references are passed to each other for co-operative control.
-     */
-    public void init() {
-        
-        /* Initialise the connection info object */
-        conInfo = new ConnectionInfo();
-        
-        guiControl = new GUIControl(tbMain, this.getParameter("host"));
-        
-        /* Create a new concontrol object */
-        conControl = new ClientConnectionControl(guiControl, conInfo);
-        
-        guiControl.setClientConnectionControl(conControl);
-        guiControl.displayGUI();
-        
-//        guiControl.statusMessage(this.getParameter("host"));
-    }
+    
+    /** Exit the Application */
+    private void exitForm(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_exitForm
+        System.exit(0);
+    }//GEN-LAST:event_exitForm
     
     /**
-     * <p>This method is called by the browser when attempting to destroy the
-     * applet. 
-     *
-     * <p>This destruction is usually caused by the browser window holding the
-     * applet being close.
-     *
-     * <p>If the applet is destroyed in this manner, disconnect from the server.
+     * @param args the command line arguments
      */
-    public void destroy() {
-        conControl.serverDisconnect("Applet closed");
+    public static void main(String args[]) {
+        new ClientFrameApp().show();
     }
     
-    public void changeSize(int width, int height) {
-
-
-        /* Redraw screen */
-        guiControl.statusMessage("Recieved resize request - now X: " + width + " and Y: " + height);
-        getRootPane().setSize(width,height);
-        getRootPane().validate();
-        
-        Dimension dim = getRootPane().getSize();
-        guiControl.statusMessage("RootPane now X: " + dim.width + " Y: " + dim.height);
-    }
-        
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTabbedPane tbMain;
     // End of variables declaration//GEN-END:variables
